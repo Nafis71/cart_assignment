5f10f9ef3d02884134d5df4b1ebd196d6d50f414
Nafis71 <nhtonmoy2@gmail.com>
2024-05-10 12:35:27 +0600

    Alert Dialog Surface Color Changed

--- lib/Widgets/shopping_card_labels.dart
+++ lib/Widgets/shopping_card_labels.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ShoppingCardLabels {
-  static Text getDressLabel(
-      {required String text, required bool isLabel}) {
+  static Text getDressLabel({required String text, required bool isLabel}) {
     if (isLabel) {
       return Text(
         text,
         style: const TextStyle(
           fontSize: 14,
           color: Colors.grey,
-          fontFamily: "WorkSans"
+          fontFamily: "WorkSans",
         ),
       );
     }
@@ -18,6 +17,7 @@ class ShoppingCardLabels {
       style: const TextStyle(
         fontSize: 14,
         color: Colors.black,
+        fontFamily: "WorkSans",
       ),
     );
   }
